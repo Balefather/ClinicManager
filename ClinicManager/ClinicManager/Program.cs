0be19dcb0a9d41aa15ad510ce0a30b2e112fc1b0
Balefather <balefather@gmail.com>
2021-11-05 11:54:09 +0100

    Project created
    Project created

---
+++ ClinicManager/ClinicManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClinicManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
